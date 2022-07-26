0ed19e85c61914189044d37d974b01a3deb75306
xprilion <xprilion@gmail.com>
2022-07-26 20:06:01 +0530

    completed styling of tickets page

--- src/pages/Tickets/Tickets.tsx
+++ src/pages/Tickets/Tickets.tsx
@@ -88,38 +88,72 @@ const Tickets = () => {
         We look forward to see you at the Cloud Community Days.
       </p>
       <p className="mb-8 lg:mb-16 font-light text-center text-red-600 sm:text-xl">
-        Workshop passes will start rolling out from 9 August. <br />
-        Check here after 9 August for your Workshop pass.
+        Workshop passes will start rolling out from 5 August. <br />
+        Check here after 5 August for your Workshop pass.
       </p>
-      <div className="w-3/4 items-center flex flex-col lg:flex-row my-0 mx-auto">
+      <div className="flex flex-col lg:flex-row my-0 lg:justify-center items-center ">
         {conferencePassTicket || workshopPassTicket ? (
           <>
 
-            {conferencePassTicket ?
-              <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <a href={conferencePassTicket} target="_blank" rel="noopener noreferrer">
-                  <img className="rounded-t-lg" src={conferencePassTicket} alt="Conference Pass" />
-                </a>
-                <div className="p-5 pt-0">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Conference Pass</h5>
+            {workshopPassTicket ?
+              <div className="m-4 flex flex-col items-center bg-gray-100 rounded-lg border shadow-md md:flex-row max-w-sm md:max-w-xl hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <img className="object-cover w-full max-h-screen border rounded-l-lg md:h-auto md:w-72 md:rounded-none md:rounded-l-lg" src={workshopPassTicket} alt="Workshop Pass" />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Workshop Pass</h5>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-
-                    &#128197; 28 August, 2022 <br />
-                    &#128205; Taal Kutir Convention Center, Kolkata
+                    &#128197; 27 August, 2022 <br />
+                    &#128205; Kolkata
                   </p>
-                  <a href={conferencePassTicket} target="_blank" rel="noopener noreferrer" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Download
-                  </a>
+                  <a href={workshopPassTicket} target="_blank" rel="noopener noreferrer"
+                    className="px-6 py-2 text-center text-white no-underline bg-blue-500 rounded hover:bg-blue-600">Download</a>
+                  <br />
+                  <span>Share the news with your friends, tag us with @GDGCloudKol (<a href="https://twitter.com/gdgcloudkol">Twitter</a>,{' '}
+                    <a href="https://facebook.com/gdgcloudkol">
+                      Facebook
+                    </a>
+                    ,{' '}
+                    <a href="https://instagram.com/gdgcloudkol">
+                      Instagram
+                    </a>{' '}
+                    or{' '}
+                    <a href="https://www.linkedin.com/company/gdgcloudkol/">
+                      LinkedIn
+                    </a>) and stand a chance to win exclusive goodies! 🎉
+                  </span>
                 </div>
               </div>
               : <></>
             }
             {
-              workshopPassTicket ? <div className="w-full lg:w-1/2">
-                <a href={workshopPassTicket} target="_blank" rel="noopener noreferrer">
-                  <img src={workshopPassTicket} alt="placeholder" />
-                </a>
-              </div> : <></>
+              conferencePassTicket ?
+                <div className="flex flex-col items-center bg-gray-100 rounded-lg border shadow-md md:flex-row max-w-sm md:max-w-xl hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                  <img className="object-cover w-full max-h-screen border rounded-l-lg md:h-auto md:w-72 md:rounded-none md:rounded-l-lg" src={conferencePassTicket} alt="Conference Pass" />
+                  <div className="flex flex-col justify-between p-4 leading-normal">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Conference Pass</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                      &#128197; 28 August, 2022 <br />
+                      &#128205; Taal Kutir Convention Center by Taj, Kolkata
+                    </p>
+                    <a href={conferencePassTicket} target="_blank" rel="noopener noreferrer"
+                      className="px-6 py-2 text-center text-white no-underline bg-blue-500 rounded hover:bg-blue-600">Download</a>
+                    <br />
+                    <span>Share the news with your friends, tag us with @GDGCloudKol (<a href="https://twitter.com/gdgcloudkol">Twitter</a>,{' '}
+                      <a href="https://facebook.com/gdgcloudkol">
+                        Facebook
+                      </a>
+                      ,{' '}
+                      <a href="https://instagram.com/gdgcloudkol">
+                        Instagram
+                      </a>{' '}
+                      or{' '}
+                      <a href="https://www.linkedin.com/company/gdgcloudkol/">
+                        LinkedIn
+                      </a>) and stand a chance to win exclusive goodies! 🎉
+                    </span>
+                  </div>
+                </div>
+
+                : <></>
             }
 
           </>
@@ -136,11 +170,16 @@ const Tickets = () => {
           </div>
         )}
       </div>
+      <br />
+      <br />
       <div className="text-center">
         <button className="btn btn-primary my-4" onClick={() => navigate('/')}>
           Go Home
         </button>
       </div>
+      <br />
+      <br />
+
     </>
   )
 }
