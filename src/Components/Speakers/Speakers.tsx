17d2c3cffdbf09f987a0ad008290a193277f9e27
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2022-07-30 00:16:20 +0530

    feat/speaker

--- src/Components/Speakers/Speakers.tsx
+++ src/Components/Speakers/Speakers.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from 'react'
 import Socials from '../Socials/Socials'
 import Title from '../Title/Title'
-import speakersData from './speakersData.json'
+// import speakersData from './speakersData.json'
 
 const Speakers = () => {
+  const [speakersDetails, setSpeakersDetails] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch('https://sessionize.com/api/v2/kirmfltc/view/Speakers')
+      .then((response) => response.json())
+      .then((data) => {
+        setSpeakersDetails(data)
+        console.log(data)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
   const imageBorderStyle = {
     // border: '1px solid #eaeaea',
     backgroundImage: `url("https://www.linkpicture.com/q/favicon-1.svg")`,
@@ -18,7 +31,7 @@ const Speakers = () => {
         className="grid sm:grid-cols-1 gap-4 md:grid-cols-3 grid-flow-row place-items-center max-w-7xl mx-auto "
         id="speakers-grid"
       >
-        {speakersData.map((speaker, id) => (
+        {speakersDetails.map((speaker, id) => (
           <div
             key={id}
             className="flex md:w-3/5 sm:w-100 flex-col rounded-2xl items-center border border-g-gray-8 p-4"
@@ -30,12 +43,12 @@ const Speakers = () => {
                 borderColor: '#3d82f8 #e5443f #f9b923 #2ea94f'
               }}
               className="inline-block h-36 w-36 rounded-full ring-2 ring-white"
-              src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              src={speaker.profilePicture}
               alt=""
             />
-            <div className="text-lg font-light mt-4">Ankit Sharma</div>
+            <div className="text-lg font-light mt-4">{speaker.fullName}</div>
             <div className="text-sm font-light mt-2 text-center">
-              Machine Learning Engineer at Carted
+            {speaker.tagLine}
             </div>
             <Socials
               twitterLink={speaker.twitterLink}
