6119c0c9a94d2d3acb723401c974cd62e93ca1d8
avinashkranjan <ranjan.avinash@hotmail.com>
2022-08-01 21:55:11 +0530

    feat/not selected added

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -10,6 +10,7 @@ const Hero = () => {
   const [user, loading]: any = useAuthState(auth)
   const [applied, setApplied] = useState(false)
   const [ticket, setTicket] = useState(false)
+  const [rejected, setRejected] = useState(false)
 
   const navigate = useNavigate()
 
@@ -24,6 +25,10 @@ const Hero = () => {
         const docRef = doc(db, 'register', user.uid)
         const docSnap = await getDoc(docRef)
         if (docSnap.exists()) {
+          console.log(docSnap.data())
+          if (docSnap.data().rejected === true) {
+            setRejected(true)
+          }
           setApplied(true)
           // navigate('/ccd2022/dashboard')
         } else {
@@ -60,8 +65,10 @@ const Hero = () => {
 
   return (
     <>
-
-      <div className="w-full max-w-7xl items-center flex flex-col lg:flex-row my-0 mx-auto gap-12 pt-20 lg:pt-28 lg:pb-[62px] px-4" id='home-grid'>
+      <div
+        className="w-full max-w-7xl items-center flex flex-col lg:flex-row my-0 mx-auto gap-12 pt-20 lg:pt-28 lg:pb-[62px] px-4"
+        id="home-grid"
+      >
         <div className="w-full lg:w-1/2">
           <div>
             <p className="text-4xl font-normal text-g-blue-3 mb-6">
@@ -81,7 +88,11 @@ const Hero = () => {
             <div className="lg:w-fit md:w-full grid grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-4">
               {user ? (
                 applied ? (
-                  ticket ? (
+                  rejected ? (
+                    <button className="transition ease-in-out bg-black duration-300 text-white h-fit w-fit text-base py-2 px-4 rounded">
+                      Not Selected
+                    </button>
+                  ) : ticket ? (
                     <button
                       className="transition ease-in-out bg-red-500 duration-300 text-white h-fit w-fit text-base py-2 px-4 rounded"
                       onClick={() => navigate('/ccd2022/tickets')}
